831f0eb8bba22a2815c24100ee985a1c99e2b062
kittichai <kittichai@kittichais-MacBook-Pro.local>
2020-05-19 00:19:41 +0700

    update

--- src/MongoDBLibrary/mongoquery.py
+++ src/MongoDBLibrary/mongoquery.py
@@ -355,10 +355,11 @@ class MongoQuery(object):
                 response = '%s%s' % (response, d.items())
             return response
 
-    def _retrieve_mongodb_records_limit(self, dbName, dbCollName, recordJSON, fields=[], recordLimit=int, returnDocuments=False):
+    def _retrieve_mongodb_records_limit(self, dbName, dbCollName, recordJSON, fields=[], recordLimit=1, returnDocuments=False):
         dbName = str(dbName)
         dbCollName = str(dbCollName)
         criteria = dict(json.loads(recordJSON))
+        recordLimit = int(recordLimit)
 
         try:
 
